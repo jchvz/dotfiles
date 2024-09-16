9331989c7f8ae1ccd036cdde934af4a31ad5322c
John Chavez <chavezjohn415@gmail.com>
2024-09-16 21:29:48 +0000

    kickstart

--- nvim/lua/config/options.lua
+++
@@ -1,6 +0,0 @@
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
-
-vim.opt.number = true
-vim.opt.relativenumber = false
-vim.opt.clipboard = 'unnamedplus'
-
