ed1b0be49257a18a52dbbb59bd98bd9af4d4b804
John Chavez <chavezjohn415@gmail.com>
2024-09-26 20:02:48 +0000

    smaller warn

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -23,19 +23,27 @@ in
   programs = {
     fish = {
       enable = true;
+      shellAliases = {
+        git_log_pretty = "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      };
       shellAbbrs = {
         # Git
         gs = "git status";
+        gl = "git_log_prety";
         gnew = "git push --set-upstream origin (git branch --show-current)";
         gmods = "git submodule deinit -f .; git submodule update --init";
+        cherry = "git cherry-pick";
+        #pr_summary = "";
 
         # Nix
         nxc = "sudo -E nvim /etc/nixos/configuration.nix";
         nxs = "sudo nixos-rebuild switch";
 
-        # Misc
+        # Transparent replacements
         cat = "bat";
         vim = "nvim";
+        grep = "rg";
+        find = "fd";
       };
     };
     fzf = {
@@ -72,6 +80,7 @@ in
     nodejs
     gnumake
     ripgrep
+    fd
     tree
     customNeovim
     zip
@@ -88,16 +97,21 @@ in
     XDG_CONFIG_HOME = "$HOME/.config";
     EDITOR = "nvim";
     BAT_THEME = "OneHalfLight";
+
+    # Fzf config
+    FZF_DEFAULT_COMMAND = "fd -H";
+    FZF_DEFAULT_OPTS = "--height 100";
+    FZF_CTRL_T_COMMAND = "fd -H";
+    FZF_CTRL_T_OPTS = ";
+      --preview 'bat -n --color=always --theme=OneHalfLight {}'
+      --bind 'ctrl-/:change-preview-window(down|hidden|)'";
+    FZF_ALT_C_OPTS = "--preview 'tree -C {}'";
   };
   # https://github.com/nix-community/NixOS-WSL
   wsl.enable = true;
   wsl.defaultUser = "john";
 
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
