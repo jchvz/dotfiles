f7c7602c82e98c36ceaa6db9d4d7bc7b26c57f12
John Chavez <chavezjohn415@gmail.com>
2024-09-28 20:29:33 +0000

    wsl clipboard

--- nvim/lua/options/vim.lua
+++ nvim/lua/options/vim.lua
@@ -5,3 +5,18 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+-- Clipboard
+vim.opt.clipboard = 'unnamedplus'
+vim.g.clipboard = {
+  name = 'WSLClipboard',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
+  },
+  paste = {
+    ['+'] = 'powershell.exe -noprofile -command Get-Clipboard',
+    ['*'] = 'powershell.exe -noprofile -command Get-Clipboard',
+  },
+  cache_enabled = 0,
+}
