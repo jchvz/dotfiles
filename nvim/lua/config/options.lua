05265141081095e99ad3bb68975d1144c8db30a7
John Chavez <chavezjohn415@gmail.com>
2024-09-30 18:56:34 +0000

    fork of lazy that actually works in nixos (no mason)

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,31 +3,31 @@ vim.opt.relativenumber = false
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Clipboard
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 local function paste()
-  local result = vim.fn.systemlist 'powershell.exe -noprofile -command Get-Clipboard'
+  local result = vim.fn.systemlist("powershell.exe -noprofile -command Get-Clipboard")
   -- Get-Clipboard adds weird `^M` artifacts
   return vim.fn.join(
     vim.fn.map(result, function(_, line)
-      return line:gsub('\r', '')
+      return line:gsub("\r", "")
     end),
-    '\n'
+    "\n"
   )
 end
 
 vim.g.clipboard = {
-  name = 'WSLClipboard',
+  name = "WSLClipboard",
   copy = {
-    ['+'] = 'clip.exe',
-    ['*'] = 'clip.exe',
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
   },
   paste = {
-    ['+'] = paste,
-    ['*'] = paste,
+    ["+"] = paste,
+    ["*"] = paste,
   },
   cache_enabled = 0,
 }
--- nvim/lua/plugins/everforest.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'sainnhe/everforest',
-  config = function()
-    vim.g.everforest_enable_italic = 0
-    vim.cmd 'colorscheme everforest'
-  end,
-}
