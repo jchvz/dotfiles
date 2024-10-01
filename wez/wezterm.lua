4e363fe522fc8549441a179cc93875bd85a25dd7
John Chavez <chavezjohn415@gmail.com>
2024-10-01 22:09:08 +0000

    not symlink but env var

--- wez/wezterm.lua
+++ wez/wezterm.lua
@@ -1,1 +1,28 @@
-/mnt/c/Users/14153/.wezterm.lua
+-- Presuppose that windows has the env var:
+-- WEZTERM_CONFIG_FILE="\\wsl$\NixOS\home\john\dotfiles\wez\wezterm.lua"
+
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.wsl_domains = {
+	-- Nix
+	{
+		name = "WSL:NixOS",
+		distribution = "NixOS",
+		default_cwd = "/home/john",
+	},
+}
+
+config.default_domain = "WSL:NixOS"
+
+config.color_scheme = "Everforest Light Hard (Gogh)"
+config.window_background_gradient = {
+	orientation = "Vertical",
+	colors = {
+		"#0f0c29",
+		"#302b64",
+	},
+}
+
+return config
