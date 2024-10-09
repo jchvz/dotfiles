0a52a016811c554b4eba68fc8c552c627ed0d8be
John Chavez <chavezjohn415@gmail.com>
2024-10-09 06:09:56 +0000

    uniform tele

--- nvim/lua/options/vim.lua
+++ nvim/lua/options/vim.lua
@@ -6,7 +6,7 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.background = 'light'
-vim.cmd('colorscheme vim') -- avoids flash of color before everforest load
+vim.cmd 'colorscheme vim' -- avoids flash of color before everforest load
 
 -- Clipboard
 vim.g.clipboard = {
@@ -16,8 +16,13 @@ vim.g.clipboard = {
     ['*'] = 'win32yank.exe -i',
   },
   paste = {
-    ['+'] = "win32yank.exe -o",
-    ['*'] = "win32yank.exe -o",
+    ['+'] = 'win32yank.exe -o',
+    ['*'] = 'win32yank.exe -o',
   },
   cache_enabled = 0,
 }
+
+-- TODO: this is a little bit aggressive, maybe find another way
+vim.api.nvim_set_keymap('n', 'y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'Y', '"+Y', { noremap = true, silent = true })
