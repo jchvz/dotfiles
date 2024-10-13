9dc3fd3366b3644ff57448ea911a5217cf676fc1
John Chavez <chavezjohn415@gmail.com>
2024-10-13 02:10:43 +0000

    nvim: golang autoimport

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -41,7 +41,36 @@ return {
 
     -- Register all LSP servers
     add_lsp('nil_ls', 'nil')
-    add_lsp('gopls', { 'gopls', 'serve' })
+    add_lsp('gopls', { 'gopls', 'serve' }, {
+      settings = {
+        gopls = {
+          gofumpt = true,
+          staticcheck = true,
+        },
+      },
+      on_attach = function()
+        -- Auto-add-imports on save
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          -- Directly taken from gopls docs:
+          -- https://github.com/golang/tools/blob/87d613117b4367083fa557b24f012ba635e5b1b6/gopls/doc/vim.md?plain=1#L171-L199
+          pattern = '*.go',
+          callback = function()
+            local params = vim.lsp.util.make_range_params()
+            params.context = { only = { 'source.organizeImports' } }
+            local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params)
+            for cid, res in pairs(result or {}) do
+              for _, r in pairs(res.result or {}) do
+                if r.edit then
+                  local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or 'utf-16'
+                  vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                end
+              end
+            end
+            vim.lsp.buf.format { async = false }
+          end,
+        })
+      end,
+    })
     add_lsp('lua_ls', { 'lua-language-server', '-E' }, {
       settings = {
         Lua = {
--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,9 +1,15 @@
 return {
-  'folke/noice.nvim',
-  event = 'VeryLazy',
-  opts = {},
-  dependencies = {
-    'MunifTanjim/nui.nvim',
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
+  },
+  {
     'rcarriga/nvim-notify',
+    config = function() end,
   },
 }
