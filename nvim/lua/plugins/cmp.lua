8526296088c03eca66cf8aee2b35da95d48e5889
John Chavez <chavezjohn415@gmail.com>
2024-10-24 17:45:56 +0000

    more sane cmp behavior

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -3,7 +3,7 @@ return {
   version = false, -- basically == master
   event = 'InsertEnter',
   dependencies = {
-    -- From the big guy
+    -- From the big guy himself
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
@@ -17,6 +17,11 @@ return {
 
     luasnip.config.setup {
       cmp.setup {
+        enabled = function()
+          -- Don't enable any snippets when I'm in a goddang comment.
+          local context = require 'cmp.config.context'
+          return (not context.in_treesitter_capture 'comment' and not context.in_treesitter_capture 'Comment')
+        end,
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
@@ -24,23 +29,29 @@ return {
         },
         completion = { completeopt = 'menu,menuone,noinsert' },
         sources = {
-          -- {
-          --   name = 'lazydev',
-          --   -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-          --   group_index = 0,
-          -- },
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
         },
-
+        formatting = {
+          format = function(entry, vim_item)
+            if not entry or not vim_item or not vim_item.kind then
+              return vim_item -- Return unchanged if nil values are found
+            end
+            -- Eliminate all "Text" completions
+            if vim_item.kind == 'Text' then
+              print 'cowabunga'
+              return nil
+            end
+            return vim_item
+          end,
+        },
         mapping = cmp.mapping.preset.insert {
-          -- basic completion menu
+          -- navigating the completion menu
           ['<Down>'] = cmp.mapping.select_next_item(),
           ['<Up>'] = cmp.mapping.select_prev_item(),
-          -- both are common for accepting completion
           ['<CR>'] = cmp.mapping.confirm { select = true },
-          ['<Tab>'] = cmp.mapping.confirm { select = true },
+          ['<Tab>'] = cmp.mapping.confirm { select = true }, -- sometimes I do this
 
           -- Scroll the documentation window [b]ack / [f]orward
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
